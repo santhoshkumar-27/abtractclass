a940933f2cd1f9171f9aebd82a08412840371390
santhoshkumar <santhoshkumar@twilightsoftwares.com>
2022-10-26 18:22:57 +0530

    nodeman installed

--- static.ts
+++ static.ts
@@ -1,12 +1,14 @@
 class stud {
     studentName: string;
     studentAge: number;
-    sudentTeacher: string;
+    // sudentTeacher: string;
+    static studentTeacher: string; // we can add static keyword in front of property name
 
     displayFn(): void {
         console.log('Student name is ', this.studentName);
         console.log('Student age is ', this.studentAge);
-        console.log('Student teacher is ', this.sudentTeacher);
+        // console.log('Student teacher is ', this.studentTeacher);
+        console.log('Student teacher is ', stud.studentTeacher);
     }
 }
 //  .*. consider
@@ -14,12 +16,46 @@ class stud {
 const s1 = new stud();
 s1.studentName = 'santhoshkumar';
 s1.studentAge = 24;
-s1.sudentTeacher = 'Mario'; // if i change the studentTeacher to the s1, we get changed only s1 not in s2
-s1.sudentTeacher = 'Sandy';
-console.log(s1.displayFn());
+// s1.studentTeacher = 'Mario'; // if i change the studentTeacher to the s1, we get changed only s1 not in s2
+// s1.studentTeacher = 'Sandy'; // now s1 has student name sandy but in the s2 we have mario, so they are same class but have different teacher
+
 
 const s2 = new stud();
 s2.studentName = 'kumar';
 s2.studentAge = 23;
-s2.sudentTeacher = 'Mario';
+// s2.studentTeacher = 'Mario';
+
+
+// so that we can implements Static method and static property for all s1 have same teacher 
+/*
+    we can implement by using two methods
+        1) using class.property;
+        2) and object.property;
+*/
+// s1.studentTeacher = 'Mario'; // not suitable in ts, but same in java
+stud.studentTeacher = 'Mario'; //using class.staticproperty we can give values
+/*
+ass$ node static.js
+Student name is  santhoshkumar
+Student age is  24
+Student teacher is  Mario
+undefined
+Student name is  kumar
+Student age is  23
+Student teacher is  Mario
+undefined
+*/
+stud.studentTeacher = 'despacito';
+/*
+ass$ node static.js
+Student name is  santhoshkumar
+Student age is  24
+Student teacher is  despacito
+undefined
+Student name is  kumar
+Student age is  23
+Student teacher is  despacito
+undefined
+*/
+console.log(s1.displayFn());
 console.log(s2.displayFn());
